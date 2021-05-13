53b8147bb8ebc3954fc9f24f70620a00eb750bb2
LucHoang <hoangsyluc@gmail.com>
2021-05-13 16:16:17 +0700

    Ứng dụng quản lý khách hàng sử dụng Spring Boot

---
+++ src/main/java/com/hsl/customermanagementspringboot/repository/ICustomerRepository.java
@@ -0,0 +1,9 @@
+package com.hsl.customermanagementspringboot.repository;
+
+import com.hsl.customermanagementspringboot.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends JpaRepository<Customer, Long> {
+}
---
+++ src/main/java/com/hsl/customermanagementspringboot/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package com.hsl.customermanagementspringboot.service;
+
+import com.hsl.customermanagementspringboot.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
